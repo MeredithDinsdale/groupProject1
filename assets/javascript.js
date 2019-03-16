026089c497cba3fc5074b898a753bbd602288a76
Abby Blubaugh <abigailrblubaugh@gmail.com>
2019-03-15 17:23:19 -0700

    add local storage&myPlaces dropdown
    local storage
    added JSON countries file
    added id to myPlacesDropdown

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,38 @@
-// $("#query").on("click", function(event) {
-//   console.log('clicked');
+$("#query").on("click", function(event) {
   $("#card").flip({
     axis: 'x',
     trigger: 'click',
   });
+});
+
+//Local Storage
+var myPlaces = JSON.parse(localStorage.getItem("myPlaces"));
+if(!myPlaces) {
+  myPlaces = [];
+}
+
+
+// Local storage click function
+$("#query").on("click", function(event) {
+  event.preventDefault();
+  var location = $("#search").val().trim();
+  myPlaces.push(location);
+  localStorage.setItem("myPlaces", JSON.stringify(myPlaces));
+
+  myPlaces = JSON.parse(localStorage.getItem("myPlaces"));
+
+  //get JSON from countries.json
+  var country;
+  $.getJSON('./assets/data/countries.json', function(countries) {         
+    country = searchObj(location.toUpperCase(), countries).name;
+    $("#myPlacesDropdown").append(`<a class="dropdown-item" href="#">` + country + `</a>`);
+  });
+});
+
+function searchObj(code, myArray){
+  for (var i=0; i < myArray.length; i++) {
+      if (myArray[i].code === code) {
+          return myArray[i];
+      }
+  }
+}
